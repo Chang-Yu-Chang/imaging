74eb81ead505322b6d60cd59347a999bf00afda2
Chang-Yu Chang <changyuchang5@gmail.com>
2025-02-05 16:18:01 -0500

    count trichome pixels

--- clean_pixsets2.R
+++ clean_pixsets2.R
@@ -1,4 +1,4 @@
-#' This script cleans the pixel sets of segmented leaf scan images
+#' This script cleans the pixel sets of segmented leaf scan images, using the trichome data
 
 library(tidyverse)
 library(imager)
@@ -7,7 +7,7 @@ library(magick)
 file_pattern <- "Tile_\\d"
 
 folder_seg <- here::here("data/trichome/segmented/") # input
-folder_temp <- here::here("data/trichome/temp/") # output
+folder_area <- here::here("data/trichome/area_trichome/") # ouput
 
 # Extract file names
 file_names <- list.files(path = folder_seg, pattern = file_pattern)
@@ -28,59 +28,59 @@ clean_pixsets <- function (file_name, file_name_extracted, save_imt = T) {
 
     cat("\nProcessing ", file_name_extracted)
     # Create a folder per raw image
-    if (!dir.exists(paste0(folder_temp, file_name_extracted))) dir.create(paste0(folder_temp, file_name_extracted))
+    if (!dir.exists(paste0(folder_area, file_name_extracted))) dir.create(paste0(folder_area, file_name_extracted))
+
+    # Optional: move the raw image to the temp folder
+    if (F) {
+        img_raw <- image_read(list.files(folder_raw, file_pattern, full.names = T))
+        image_write(img_raw, paste0(folder_out, file_name_extracted, "-00-raw.png"))
+    }
 
     # Move the segmented image to the temp folder
     img_seg <- load.image(paste0(folder_seg, file_name)) # This is the default input
-    save.image(img_seg, paste0(folder_temp, file_name_extracted, "/01-seg.png"))
+    save.image(img_seg, paste0(folder_area, file_name_extracted, "/01-seg.png"))
     cat("\tSegmentation")
 
     # 1. Manual crop
     img_cropped <- as.cimg(img_seg[400:2900, 600:3400])
-    save.image(img_cropped, paste0(folder_temp, file_name_extracted, "/02-cropped.png"))
+    save.image(img_cropped, paste0(folder_area, file_name_extracted, "/02-cropped.png"))
 
     # 2. Threshold
-    ps_bin <- img_cropped < threshold(img_cropped)
+    ps_bin <- img_cropped > 0.8
     img_bin <- as.cimg(ps_bin)
-    if (save_imt) save.image(img_bin, paste0(folder_temp, file_name_extracted, "/03-binary.png"))
+    if (save_imt) save.image(img_bin, paste0(folder_area, file_name_extracted, "/03-binary.png"))
     cat("\tBinary")
 
+    # Binary mask
+    binary_mask <- load.image(paste0(, file_name))
     # 3. Create a new binary image containing only large objects
     img_labeled <- label(ps_bin)
     tab <- table(img_labeled)
-    min_size <- 10000
+    min_size <- 100
 
     large_objects <- names(which(tab >= min_size))
     large_objects <- large_objects[-1] # remove the background
     img_large <- is.element(img_labeled, large_objects) %>% array(dim = dim(img_bin)) %>% as.cimg()
-    if (save_imt) save.image(img_large, paste0(folder_temp, file_name_extracted, "/04-large.png"))
+    if (save_imt) save.image(img_large, paste0(folder_area, file_name_extracted, "/04-large.png"))
     cat("\tKept large pixsets")
 
 
     # 4. Fill the pixsets
     img_eroded <- erode_square(img_large, 2)
-    if (save_imt) save.image(img_eroded, paste0(folder_temp, file_name_extracted, "/05-eroded.png"))
+    if (save_imt) save.image(img_eroded, paste0(folder_area, file_name_extracted, "/05-eroded.png"))
     cat("\t Eroded")
     img_dilated <- dilate_square(img_eroded, 6)
-    if (save_imt) save.image(img_dilated, paste0(folder_temp, file_name_extracted, "/06-dilated.png"))
+    if (save_imt) save.image(img_dilated, paste0(folder_area, file_name_extracted, "/06-dilated.png"))
     cat("\tDilated")
     img_filled <- bucketfill(img_dilated, 1, 1, color = 2) %>% {!( . == 2) } %>% as.cimg()
-    save.image(img_filled, paste0(folder_temp, file_name_extracted, "/07-filled.png"))
+    save.image(img_filled, paste0(folder_area, file_name_extracted, "/07-filled.png"))
     cat("\tFilled")
-
-    # 5. Extract the trichome pixel sets
-    ## Prepare a mask that only cover the leaf
-    img_mask <- dilate_square(img_filled, 50)
-    save.image(img_mask, paste0(folder_temp, file_name_extracted, "/08-mask.png"))
-    ## Subset the pixels
-    ps_tri <- (img_cropped * img_mask) > 0.8
-    img_tri <- as.cimg(ps_tri)
-    save.image(img_tri, paste0(folder_temp, file_name_extracted, "/09-trichome.png"))
-    ## Remove small pixsets
-    img_tricle <- clean(img_tri, 2) %>% as.cimg()
-    save.image(img_tricle, paste0(folder_temp, file_name_extracted, "/10-trichome_cleaned.png"))
-
 }
+
+i = 1
+file_name <- file_names[i]
+file_name_extracted <- file_names_extracted[i]
+clean_pixsets(file_names[i], file_names_extracted[i])
 
 
 for (i in 1:length(file_names)) clean_pixsets(file_names[i], file_names_extracted[i], save_imt = T)
--- scripts/extract_area.R
+++ scripts/extract_area.R
@@ -6,26 +6,22 @@ library(imager)
 file_names <- list.files(path = here::here("data/trichome/temp"), full.names = T)
 file_names_extracted <- str_extract(file_names, "Tile_\\d+")
 
-count_fore_pixels <- function(img) {
-    #' The input must be a binary image, ideally a filled image
-    img_labeled <- label(img)
-    tab <- table(img) %>% as_tibble() %>% rename(pixeltype = img) %>%
-        mutate(pixeltype = case_when(
-            pixeltype == 0 ~ "background",
-            pixeltype == 1 ~ "foreground"
-        ))
-    return(tab)
+count_fore_pixels <- function(file_name) {
+    #' The input must be a binary image
+    img_leaf <- load.image(paste0(file_name, "/07-filled.png"))
+    img_trichome <- load.image(paste0(file_name, "/10-trichome_cleaned.png"))
+
+    return(tibble(leaf = sum(img_leaf), trichome = sum(img_trichome)))
 }
 
 counts <- list()
 for (i in 1:length(file_names)) {
     file_name <- file_names[i]
-    img <- load.image(paste0(file_name, "/07-filled.png"))
-    counts[[file_names_extracted[i]]] <- count_fore_pixels(img)
+    counts[[file_names_extracted[i]]] <- count_fore_pixels(file_name)
     cat("\t", i)
 }
 
-leaf_area <- bind_rows(counts, .id = "sample") %>%
-    pivot_wider(names_from = pixeltype, values_from = n)
+areas <- bind_rows(counts, .id = "sample") %>%
+    mutate(trichome_to_leaf = trichome / leaf)
 
-write_csv(leaf_area, here::here("data/trichome/leaf_area.csv"))
+write_csv(areas, here::here("data/trichome/areas.csv"))
